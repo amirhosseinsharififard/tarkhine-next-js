672aed0a30e97934dca8824873e94e65a924bf5e
Amir Hossin Sharifi Fard <amirawade7@gmail.com>
2025-05-16 16:59:16 +0330

    configure tailwind css styles tailwind config install packages
    create responsive header (mobile first)

---
+++ src/components/layout/Header.js
@@ -0,0 +1,85 @@
+import Link from "next/link";
+
+//  image and svg
+
+import Image from "next/image";
+import menu from "../../../public/images/menu.svg";
+import profileIcon from "../../../public/images/ProfileIcon.svg";
+import logo from "../../../public/images/Logo.png";
+import headerIcon from "../../../public/images/HeaderIcon.svg";
+import searchIcon from "../../../public/images/Search.svg";
+import logoWeb from "../../../public/images/LogoWeb.svg";
+
+const Header = () => {
+  return (
+    <div className='max-w-screen-xl m-auto '>
+      <div className='flex justify-between items-center p-[20px] lg:pt-32  mx-auto'>
+        <div className='flex items-center'>
+          <Link href='/profile'>
+            <Image
+              src={profileIcon}
+              alt='Profile Icon'
+              className='w-[24px] h-[24px] lg:w-[40px] lg:h-[40px]'
+            />
+          </Link>
+          <Link href='/header' className='mx-[2px] lg:mx-[4px]'>
+            <Image
+              src={headerIcon}
+              alt='Header Icon'
+              className='w-[24px] h-[24px] lg:w-[40px] lg:h-[40px]'
+            />
+          </Link>
+          <Link href='/search' className='hidden lg:block'>
+            <Image
+              src={searchIcon}
+              alt='search Icon'
+              className='lg:w-[40px] lg:h-[40px]'
+            />
+          </Link>
+        </div>
+
+        <div>
+          <Link href={"/"} className='lg:hidden'>
+            <Image src={logo} alt='Logo' width={120} height={40} />
+          </Link>
+          <ul className='hidden lg:flex flex-row-reverse gap-24 items-center'>
+            <li className='text-h5 text-main-primary underline underline-offset-8 decoration-1 '>
+              <Link href='/'>صفحه اصلی</Link>
+            </li>
+            <li className='text-bodyXL'>
+              <Link href='/'>شعبه</Link>
+            </li>
+            <li className='text-bodyXL'>
+              <Link href='/'>منو</Link>
+            </li>
+            <li className='text-bodyXL'>
+              <Link href='/'> اعطای نمایندگی</Link>
+            </li>
+            <li className='text-bodyXL'>
+              <Link href='/'>درباره ما</Link>
+            </li>
+            <li className='text-bodyXL'>
+              <Link href='/'>تماس با ما</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <Link href='/header' className='lg:hidden'>
+            <Image src={menu} alt='Menu Icon' width={24} height={24} />
+          </Link>
+
+          <Link href='/' className='hidden lg:block'>
+            <Image
+              src={logoWeb}
+              alt='Menu Icon'
+              className='w-[155px] h-[51px]'
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,195 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // مسیرهای Next.js
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        estedad: ["Estedad", "sans-serif"], // اضافه کردن فونت
+      },
+      colors: {
+        main: {
+          tint1: "#E5F2E9",
+          tint2: "#CAE4D3",
+          tint3: "#B0D7BD",
+          tint4: "#96C9A7",
+          tint5: "#7CBC91",
+          tint6: "#61AE7B",
+          tint7: "#4E9968",
+          primary: "#417F56",
+          shade1: "#396F56",
+          shade2: "#315F41",
+          shade3: "#294F36",
+          shade4: "#21402B",
+          shade5: "#183020",
+          shade6: "#102016",
+          shade7: "#08100B",
+        },
+        neutral: {
+          white: "#FFFFFF",
+          gray1: "#F9F9F9",
+          gray2: "#E1E1E1",
+          gray3: "#EDEDED",
+          gray4: "#CBCBCB",
+          gray5: "#ADADAD",
+          gray6: "#757575",
+          gray7: "#717171",
+          gray8: "#353535",
+          black: "#0C0C0C",
+        },
+        status: {
+          error: "#C30000",
+          errorL: "#ED2E2E",
+          errorEL: "#FFF2F2",
+          success: "#00966D",
+          successL: "#00BA88",
+          successEL: "#F3FDA",
+          warning: "#A9791C",
+          warningL: "#F4B740",
+          warningEL: "#FFF8E1",
+        },
+      },
+      padding: {
+        8: "8px",
+        16: "16px",
+        24: "24px",
+        35: "35px",
+      },
+      boxShadow: {
+        2: "0 2px 2px rgba(0, 0, 0, 0.05)",
+        4: "0 4px 4px rgba(0, 0, 0, 0.05)",
+        6: "0 6px 6px rgba(0, 0, 0, 0.05)",
+        8: "0 8px 8px rgba(0, 0, 0, 0.05)",
+        12: "0 12px 12px rgba(0, 0, 0, 0.05)",
+        16: "0 16px 16px rgba(0, 0, 0, 0.05)",
+        cards:
+          "0 25px 7px rgba(0, 0, 0, 0),0 16px 6px rgba(0, 0, 0, 0.01),0 9px 5px rgba(0, 0, 0, 0.05),0 4px 4px rgba(0, 0, 0, 0.09),0 1px 2px rgba(0, 0, 0, 0.1),0 0px 0px rgba(0, 0, 0, 0.1)",
+        contactCard:
+          "0 66px 18px rgba(0, 0, 0, 0),0 42px 17px rgba(0, 0, 0, 0.01),0 24px 14px rgba(0, 0, 0, 0.05),0 11px 11px rgba(0, 0, 0, 0.09),0 3px 6px rgba(0, 0, 0, 0.1),0 0px 0px rgba(0, 0, 0, 0.1)",
+      },
+      borderRadius: {
+        4: "4px",
+        8: "8px",
+        16: "16px",
+        24: "24px",
+        32: "32px",
+        64: "64px",
+      },
+      spacing: {
+        8: "8px ",
+        16: "16px ",
+        24: "24px ",
+        32: "32px ",
+        40: "40px ",
+        48: "48px ",
+        56: "56px ",
+        64: "64px ",
+        80: "80px ",
+        120: "120px ",
+        160: "160px ",
+      },
+      text: {
+        display1: {
+          fontSize: "64px",
+          fontWeight: "bold",
+          lineHeight: "140%",
+        },
+        display2: {
+          fontSize: "56px",
+          fontWeight: "bold",
+          lineHeight: "140%",
+        },
+        h1: {
+          fontSize: "44px",
+          fontWeight: "bold",
+          lineHeight: "140%",
+        },
+        h2: {
+          fontSize: "40px",
+          fontWeight: "bold",
+          lineHeight: "140%",
+        },
+        h3: {
+          fontSize: "32px",
+          fontWeight: "bold",
+          lineHeight: "140%",
+        },
+        h4: {
+          fontSize: "24px",
+          fontWeight: "bold",
+          lineHeight: "140%",
+        },
+        h5: {
+          fontSize: "20px",
+          fontWeight: "bold",
+          lineHeight: "140%",
+        },
+        h6: {
+          fontSize: "16px",
+          fontWeight: "semibold",
+          lineHeight: "180%",
+        },
+        bodyXL: {
+          fontSize: "20px",
+          fontWeight: "regular",
+          lineHeight: "180%",
+        },
+        bodyLG: {
+          fontSize: "18px",
+          fontWeight: "regular",
+          lineHeight: "180%",
+        },
+        bodyMD: {
+          fontSize: "12px",
+          fontWeight: "regular",
+          lineHeight: "180%",
+        },
+        bodySM: {
+          fontSize: "14px",
+          fontWeight: "regular",
+          lineHeight: "180%",
+        },
+        captionMD: {
+          fontSize: "12px",
+          fontWeight: "regular",
+          lineHeight: "180%",
+        },
+        captionLG: {
+          fontSize: "14px",
+          fontWeight: "bold",
+          lineHeight: "180%",
+        },
+        captionSM: {
+          fontSize: "10px",
+          fontWeight: "bold",
+          lineHeight: "180%",
+        },
+        buttonLG: {
+          fontSize: "16px",
+          fontWeight: "medium",
+          lineHeight: "180%",
+        },
+        buttonSM: {
+          fontSize: "14px",
+          fontWeight: "medium",
+        },
+        overlineLG: {
+          fontSize: "16px",
+          fontWeight: "regular",
+          lineHeight: "180%",
+        },
+        overlineSM: {
+          fontSize: "12px",
+          fontWeight: "bold",
+          lineHeight: "auto%",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
