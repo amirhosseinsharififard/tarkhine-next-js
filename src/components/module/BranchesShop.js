9f253d392b9d89b673b391bf4be50671e06803b4
AmirHosseinSharifiFard <amirhosseinsharififard@gmail.com>
2025-07-04 19:04:46 +0330

    add icons and responsive styles lets add comments

--- src/components/module/BranchesShop.js
+++ src/components/module/BranchesShop.js
@@ -1,6 +1,11 @@
-import { e2p, formatPrice } from "@/utils/replaceNumber";
+import { e2p} from "@/utils/replaceNumber";
 import SwiperSlider from "./Swiper";
+import Image from "next/image";
 
+// icons 
+import call from '@/public/images/svg/call-calling.svg'
+import clock from '@/public/images/svg/clock.svg'
+import location from '@/public/images/svg/location.svg'
 export default function BranchesShop() {
   return (
     <div className="text-center">
@@ -9,28 +14,30 @@ export default function BranchesShop() {
       </h4>
       <SwiperSlider slider="bigSlider" branch={true} />
       <div className="relative text-neutral-gray8">
-        <div className="flex flex-col p-3 border-1 lg:border-3 border-main-primary w-[320px] mx-auto  absolute top-[-20px] lg:top-[-30px] left-0 right-0 bg-neutral-white z-20  gap-2">
-          <div className="flex">
-            <span>icon</span>
-
-            <p className="text-captionSM lg:text-bodyMD ">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-2 p-3 border border-main-primary w-[320px] lg:w-[810px] mx-auto absolute top-[-20px] lg:top-[-30px] left-0 right-0 bg-neutral-white z-20">
+          <div className="col-span-2 lg:col-span-1 flex lg:flex-col order-1 lg:order-2">
+            <Image src={location} alt="location"  className="lg:mx-auto ml-1  w-[16px] h-[16px] lg:w-[32px] lg:h-[32px] " />
+            <p className="text-captionSM lg:text-bodyMD">
               شهرک اکباتان، فاز ۳، مجتمع تجاری کوروش، طبقه سوم
             </p>
           </div>
-          <div className="flex items-center content-center justify-between">
-            <div className="flex">
-              <span>icon</span>
-
-              <p className="text-captionSM lg:text-bodyMD ">
-                {`${e2p(21)}-${e2p(54891250)}-${e2p(51)}`}
-              </p>
-            </div>
-            <div className="flex">
-              <span>icon</span>
-              <p className="text-captionSM lg:text-bodyMD ">
-                همه‌روزه از ساعت ۱۲ الی ۲۳
-              </p>
-            </div>
+          <div className="flex lg:flex-col order-2 lg:order-1">
+            <Image src={call} alt="call" className="lg:mx-auto ml-1  w-[16px] h-[16px] lg:w-[32px] lg:h-[32px] "  />
+            <p className="lg:hidden text-captionSM lg:text-bodyMD">
+              {`${e2p(21)}-${e2p(54891250)}-${e2p(51)}`}
+            </p>
+            <p className="hidden lg:block lg:text-bodyMD">
+              {`${e2p(21)}-${e2p(33535354)}`}
+            </p>
+            <p className="hidden lg:block lg:text-bodyMD">
+              {`${e2p(21)}-${e2p(33535356)}`}
+            </p>
+          </div>
+          <div className="flex lg:flex-col order-3 lg:order-3">
+            <Image src={clock} alt="clock"  className="lg:mx-auto ml-1  w-[16px] h-[16px] lg:w-[32px] lg:h-[32px] " />
+            <p className="text-captionSM lg:text-bodyMD">
+              همه‌روزه از ساعت ۱۲ الی ۲۳
+            </p>
           </div>
         </div>
       </div>
