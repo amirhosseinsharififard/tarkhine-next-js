323fae66110e2ae5e45ed5870650660948875377
AmirHosseinSharifiFard <amirhosseinsharififard@gmail.com>
2025-07-06 18:07:10 +0330

    res

--- src/components/module/CardMenuResturant.js
+++ src/components/module/CardMenuResturant.js
@@ -6,18 +6,22 @@ import imageCardMenu from "@/public/images/home/imageCardMenu.png";
 
 export default function CardMenuResturant() {
   return (
-    <div className="mb-20 col-span-2 md:col-span-1 w-[152px]">
+    <div className="mb-20 col-span-2 md:col-span-1 w-[clamp(152px,15vw,287px)]">
       <div className="relative grid">
         <Image
           src={imageCardMenu}
           alt="imageCardMenu"
-          className="w-[120px] h-[120px] rounded-full z-1 m-auto shadow-8"
+          className="w-[clamp(120px,12vw,240px)] h-[clamp(120px,12vw,240px)] rounded-full z-1 m-auto shadow-8"
         />
-        <div className="absolute bottom-[-20] left-0 right-0 bg-main-primary h-[80px] rounded-4">
+        <div
+          className="absolute bottom-[clamp(-25px,2vw,-40px)] left-0 right-0 bg-main-primary h-[clamp(80px,6vw,160px)]
+            rounded-4"
+        >
           <p
-            className="absolute bottom-[-22] left-0 right-0  z-2 shadow-4 m-auto 
-            w-[96px] h-[32px] rounded-4 bg-neutral-gray1 text-captionMD 
-             flex items-center justify-center"
+            className="absolute bottom-[-20] left-0 right-0  z-2 shadow-4 m-auto 
+              h-[clamp(32px,6vw,48px)]
+              w-[clamp(96px,9vw,155px)] rounded-4 bg-neutral-gray1 text-captionMD md:text-bodySM lg:text-bodyXL
+              flex items-center justify-center"
           >
             غذای اصلی
           </p>
--- src/components/module/MenuResturant.js
+++ src/components/module/MenuResturant.js
@@ -8,7 +8,7 @@ import imageCardMenu from "@/public/images/home/imageCardMenu.png";
 export default function MenuResturant() {
   return (
     <div>
-      <h4 className="text-h6 text-center">منوی رستوران</h4>
+      <h4 className="text-h6 text-center md:text-h5 lg:text-h4 lg:mt-6 lg:mb-8">منوی رستوران</h4>
       <div className="grid grid-cols-4 place-items-center mx-2">
       
         <CardMenuResturant />
