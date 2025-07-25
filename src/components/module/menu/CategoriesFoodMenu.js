6cea0d7c038efe7d083c197eb9f6a5b4bb00ce6a
Amir Sharifi <amir@example.com>
2025-07-25 20:17:54 +0330

    change some codes to be responsive

--- src/components/module/menu/CategoriesFoodMenu.js
+++ src/components/module/menu/CategoriesFoodMenu.js
@@ -8,19 +8,19 @@ import { IoCartOutline } from "react-icons/io5";
 export default function CategoriesFoodMenu({ title }) {
   const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
-    <div className="col-span-12 px-2 md:px-2 mt-6 sm:mt-12">
+    <div className="col-span-12  px-2 md:px-5 mt-3 ">
       <div className="flex items-center justify-between content-center text-nowrap my-4 sm:my-6">
         <h4 className="text-h6 text-neutral-gray8 md:text-h4">{title}</h4>
         <CustomeButton
           model="stroke"
           classname={
-            "flex !w-auto text-nowrap gap-2.5 text-captionMD sm:text-buttonLG items-center"
+            "flex !w-auto  text-nowrap gap-1 text-captionMD sm:text-buttonLG items-center !h-[32px]"
           }
         >
           <span>
             <IoCartOutline className="w-4 h-4 sm:w-6 sm:h-6 text-main-primary" />
-          </span>{" "}
-          <p>تکمیل سفارش</p>
+          </span>
+          <p className="text-captionMD">تکمیل سفارش</p>
         </CustomeButton>
       </div>
 
